8b4e4f6129b4213a368da0137409f2d7cabcffd9
shadid13 <shaque@chisel.ai>
2018-07-10 16:42:34 -0400

    added new stuff to the migration charts

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -13,13 +13,38 @@ import Collapse from '@material-ui/core/Collapse';
 import Divider from '@material-ui/core/Divider';
 import StarBorder from '@material-ui/icons/StarBorder';
 
+import {
+    BarChart, 
+    Bar, 
+    ReferenceLine, 
+    XAxis, 
+    YAxis, 
+    CartesianGrid, 
+    Tooltip, 
+    Legend
+
+} from 'recharts';
+
 // css
 import './Profile.css';
 import logo from './logo.png';
 
 // Components
 import TopBar from './TopBar/TopBar';
-import Loader from '../Loader/Loader'
+import Loader from '../Loader/Loader';
+import DashLineChart from '../DashLineChart/DashLineChart'
+
+
+const data = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: -3000, pv: 1398, amt: 2210},
+    {name: 'Page C', uv: -2000, pv: -9800, amt: 2290},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page E', uv: -1890, pv: 4800, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: -3800, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
 
 class Profile extends Component {
     state = { open: true, loading: true };
@@ -31,7 +56,7 @@ class Profile extends Component {
     componentDidMount() {
         setTimeout(()=> {
             this.setState({ loading: false })
-        }, 5000)
+        }, 1000)
     }
 
     render() {
@@ -92,7 +117,26 @@ class Profile extends Component {
                     {
                         this.state.loading ? (
                             <Loader />
-                        ) : null
+                        ) : (
+                            <div>
+                                <div className='chart-container'>
+                                    <DashLineChart />
+                                </div>
+                                <div className='achart-container'>
+                                <BarChart width={600} height={300} data={data}
+                                        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                                    <CartesianGrid strokeDasharray="3 3"/>
+                                    <XAxis dataKey="name"/>
+                                    <YAxis/>
+                                    <Tooltip/>
+                                    <Legend />
+                                    <ReferenceLine y={0} stroke='#000'/>
+                                    <Bar dataKey="pv" fill="#8884d8" />
+                                    <Bar dataKey="uv" fill="#82ca9d" />
+                                </BarChart>
+                                </div>
+                            </div>
+                        )
                     }
                 </div>
             </div>
