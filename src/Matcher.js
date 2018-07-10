326d8a81470abeba5c2f9bec06454225cb67ada6
shadid12 <shadidhaque2014@gmail.com>
2018-07-10 02:18:37 -0400

    problem with sibling states

--- src/Matcher.js
+++ src/Matcher.js
@@ -315,7 +315,7 @@ class Matcher extends Component {
                                 );
 
                               }}
-                              highlights={appliedHighlights}
+                              highlights={highlights}
                             />
                           )
                         }
